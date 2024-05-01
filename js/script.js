1e372fc675f8ec81fe0aca7ab8d08462e39187cf
Floomin <floomin@gmail.com>
2024-05-01 21:50:40 +0300

    Добавил звонок в телеграм

--- js/script.js
+++ js/script.js
@@ -61,3 +61,55 @@ prev.addEventListener("click", function () {
   }
   imgs[current_slide].style.opacity = 1;
 });
+
+
+// Send massege to Telegram
+const TelegramBotToken = '7078321427:AAHAwtID5m9wf-xgoA-SvZHFy3d7kkF6YmA';
+const TelegramChatId = '@TheALLEYgroup';
+const API = `https://api.telegram.org/bot${TelegramBotToken}/sendMessage`;
+
+async function sendEmailTelegram() {
+  event.preventDefault();
+  // return false
+
+  const form = event.currentTarget;
+
+  // Need sbumit
+  const formBtn = document.querySelector('.btn');
+  const formSendResult = document.querySelector('.form__send-result');
+  formSendResult.textContent = '';
+
+  const { name, phone } = Object.fromEntries(new FormData(form).entries());
+  const text = `Заявка від ${name},\n  ${phone}`;
+
+  try {
+    formBtn.textContent = 'Відправка повідомлення'
+    const response = await fetch(API, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: TelegramChatId,
+        text
+      })
+    });
+
+    if (response.ok) {
+      formSendResult.textContent = 'Дякуємо за запит, наші спеціалісти зв`яжутся з вами наближчим часом.';
+      form.reset();
+    }
+    else {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    console.error(error);
+    formSendResult.textContent = 'Вибачте сервіс тимчасово не працює, спробуйте будь-ласка пізніше';
+    formSendResult.style.color = 'red'
+  }
+  finally {
+    formBtn.textContent = 'Перездвоніть мені';
+    setInterval(closeModal, 1500);
+  }
+
+}
